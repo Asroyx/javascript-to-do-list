f7394272c891cbef14a776335b6741da8801bcfc
Asroyx <mystoganza@hotmail.com>
2023-11-04 01:41:49 +0300

    Refactor the newElement function for improved efficiency and readability

--- js/index.js
+++ js/index.js
@@ -1,46 +1,32 @@
 function newElement() {
   const task = document.getElementById("task").value;
+  const toastError = document.getElementById("liveToastError");
+  const toastAdd = document.getElementById("liveToastAdd");
+  const list = document.getElementById("list");
 
-  if (task == "") {
-    const toastError = document.getElementById("liveToastError");
-    toastError.classList.remove("hide");
-    toastError.classList.add("show");
-
-    setTimeout(function () {
-      toastError.classList.remove("show");
-      toastError.classList.add("hide");
-    }, 2000);
+  if (task === "") {
+    toastError.classList.replace("hide", "show");
+    setTimeout(() => toastError.classList.replace("show", "hide"), 2000);
     return;
   }
 
-  const list = document.getElementById("list");
   const newElement = document.createElement("li");
   newElement.textContent = task;
   list.appendChild(newElement);
 
   const span = document.createElement("SPAN");
-
   span.className = "close";
   span.textContent = "\u00D7";
-
   newElement.appendChild(span);
 
-  span.addEventListener("click", () => {
-    list.removeChild(newElement);
-  });
+  span.addEventListener("click", () => list.removeChild(newElement));
 
-  const toastAdd = document.getElementById("liveToastAdd");
-  toastAdd.classList.remove("hide");
-  toastAdd.classList.add("show");
+  toastAdd.classList.replace("hide", "show");
+  setTimeout(() => toastAdd.classList.replace("show", "hide"), 2000);
 
   list.addEventListener("click", (e) => {
     e.target.classList.toggle("checked");
   });
-
-  setTimeout(function () {
-    toastAdd.classList.remove("show");
-    toastAdd.classList.add("hide");
-  }, 2000);
 
   document.getElementById("task").value = "";
 }
