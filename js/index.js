7fe60172e732be693d6a9687d99e2c1be2796edf
Asroyx <mystoganza@hotmail.com>
2023-11-04 00:05:57 +0300

    Add validation for empty task input

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 function newElement() {
   const task = document.getElementById("task").value;
 
+  if (task == "") {
+    const toast = document.getElementById("liveToastError");
+    toast.classList.remove("hide");
+    toast.classList.add("show");
+    return;
+  }
+
   const list = document.getElementById("list");
   const newElement = document.createElement("li");
   newElement.textContent = task;
