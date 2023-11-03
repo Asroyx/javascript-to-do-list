65d258f2c8c9d9066620c7f58c8f186d7a8fb1db
Asroyx <mystoganza@hotmail.com>
2023-11-04 00:32:53 +0300

    Add timeout to hide toast

--- js/index.js
+++ js/index.js
@@ -5,6 +5,11 @@ function newElement() {
     const toastError = document.getElementById("liveToastError");
     toastError.classList.remove("hide");
     toastError.classList.add("show");
+
+    setTimeout(function () {
+      toastError.classList.remove("show");
+      toastError.classList.add("hide");
+    }, 2000);
     return;
   }
 
@@ -16,6 +21,11 @@ function newElement() {
   const toastAdd = document.getElementById("liveToastAdd");
   toastAdd.classList.remove("hide");
   toastAdd.classList.add("show");
+
+  setTimeout(function () {
+    toastAdd.classList.remove("show");
+    toastAdd.classList.add("hide");
+  }, 2000);
 
   document.getElementById("task").value = "";
 }
