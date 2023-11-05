e7bb7a4220cbcdd5f0339cf8c4869a53b7c90aad
Asroyx <mystoganza@hotmail.com>
2023-11-05 14:49:04 +0300

    chore: Update click functions and save to Local Storage

--- js/index.js
+++ js/index.js
@@ -1,8 +1,35 @@
 const list = document.getElementById("list");
 
-list.addEventListener("click", (e) => {
-  e.target.classList.toggle("checked");
-});
+function populateList() {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  for (let i = 0; i < tasks.length; i++) {
+    const newElement = document.createElement("li");
+    newElement.textContent = tasks[i].task;
+    if (tasks[i].checked) {
+      newElement.classList.add("checked");
+    }
+
+    const span = document.createElement("SPAN");
+    span.className = "close";
+    span.textContent = "\u00D7";
+    newElement.appendChild(span);
+
+    span.addEventListener("click", () => {
+      list.removeChild(newElement);
+      tasks.splice(i, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    });
+
+    newElement.addEventListener("click", () => {
+      newElement.classList.toggle("checked");
+      tasks[i].checked = !tasks[i].checked;
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    });
+
+    list.appendChild(newElement);
+  }
+}
+populateList();
 
 function newElement() {
   const task = document.getElementById("task").value;
@@ -15,6 +42,10 @@ function newElement() {
     return;
   }
 
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.push({ task: task, checked: false });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
   const newElement = document.createElement("li");
   newElement.textContent = task;
   list.appendChild(newElement);
@@ -24,7 +55,19 @@ function newElement() {
   span.textContent = "\u00D7";
   newElement.appendChild(span);
 
-  span.addEventListener("click", () => list.removeChild(newElement));
+  span.addEventListener("click", () => {
+    list.removeChild(newElement);
+    const index = tasks.findIndex((item) => item.task === task);
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  newElement.addEventListener("click", () => {
+    newElement.classList.toggle("checked");
+    const index = tasks.findIndex((item) => item.task === task);
+    tasks[index].checked = !tasks[index].checked;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
 
   toastAdd.classList.replace("hide", "show");
   setTimeout(() => toastAdd.classList.replace("show", "hide"), 2000);
